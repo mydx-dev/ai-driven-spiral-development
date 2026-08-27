4ead29dd487d63e0304da43e663d2bbc106d5e5e
tiger-oshima <oshima.automate.growth@gmail.com>
2026-08-27 15:11:11 +0900

    test: Standard packageをroot testから分離する

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    exclude: ["dist/**", "node_modules/**", "packages/quality/**"],
+    exclude: [
+      "dist/**",
+      "node_modules/**",
+      "packages/quality/**",
+      "packages/standard/**",
+    ],
   },
 });
