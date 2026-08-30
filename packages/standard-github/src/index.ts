1848ad54ee7a420b30c70af4295525893390b01f
tiger-oshima <oshima.automate.growth@gmail.com>
2026-08-30 12:45:25 +0900

    feat: export Standard Artifact GitHub adapter

--- packages/standard-github/src/index.ts
+++ packages/standard-github/src/index.ts
@@ -2,3 +2,5 @@ export * from "./IssueMappings.js";
 export * from "./IssueTemplates.mjs";
 export * from "./Repositories.js";
 export * from "./Runtime.js";
+export * from "./StandardArtifactIssueCodecs.js";
+export * from "./StandardArtifactIssueRepository.js";
