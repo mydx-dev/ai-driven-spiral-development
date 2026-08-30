16973bdca0a68227be220098fc8d2ff7196b2263
tiger-oshima <oshima.automate.growth@gmail.com>
2026-08-31 07:17:27 +0900

    feat: add software element execution planning

---
+++ packages/standard/src/orchestration/SoftwareElementExecutionPlan.ts
@@ -0,0 +1,18 @@
+import type { SoftwareArchitectureDescription } from "../artifact/SoftwareArchitectureDescription.js";
+
+export class SoftwareElementExecutionPlan {
+  constructor(public readonly architecture: SoftwareArchitectureDescription) {}
+
+  executableElementIds(completedElementIds: Iterable<string>): string[] {
+    const completed = new Set(completedElementIds);
+    const dependencyGraph = this.architecture.dependencyGraph();
+
+    return [...dependencyGraph.entries()]
+      .filter(
+        ([elementId, dependencies]) =>
+          !completed.has(elementId) &&
+          [...dependencies].every((dependencyId) => completed.has(dependencyId)),
+      )
+      .map(([elementId]) => elementId);
+  }
+}
